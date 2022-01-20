0dd2aba1938906119e70722452ae95bf8981b285
VinnieMaen <52604982+VinnieMaen@users.noreply.github.com>
2022-01-20 20:35:26 +0100

    Create 11.java

---
+++ Examen/11.java
@@ -0,0 +1,3 @@
+//Antwoord a is correct.
+//b is niet juist want voeder IS gedefinieerd in klasse dier.
+//c is niet juist want niet-interfaces kunnen ook abstracte methoden hebben. Interfaces hebben ENKEL abstracte methoden.
