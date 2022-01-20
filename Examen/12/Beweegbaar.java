8cf15ef10df29a04c53222483821351b9268387c
VinnieMaen <52604982+VinnieMaen@users.noreply.github.com>
2022-01-20 20:47:53 +0100

    Add files via upload

---
+++ Examen/12/Beweegbaar.java
@@ -0,0 +1,4 @@
+
+public interface Beweegbaar {
+	void beweeg () ;
+}
---
+++ Examen/12/Mens.java
@@ -0,0 +1,12 @@
+
+public class Mens {
+	private String naam;
+	
+	public Mens ( String n ) {
+		naam = n ;
+	}
+	
+	public static void laatBewegen(Mens m) {
+		System.out.println(m.naam + " beweegt");
+	}
+}
